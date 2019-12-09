ea48bbc8cd18041a98de76d9373080f7e8125b2a
Keir Badger <keir.badger@acuris.com>
2019-12-09 15:04:45 +0000

    Upgrade module to Terraform 0.12

--- variables.tf
+++ variables.tf
@@ -4,130 +4,131 @@ variable "env" {
 
 variable "platform_config" {
   description = "Platform configuration"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "release" {
-  type        = "map"
+  type        = map(string)
   description = "Metadata about the release"
 }
 
 variable "common_application_environment" {
   description = "Environment parameters passed to the container for all environments"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "application_environment" {
   description = "Environment specific parameters passed to the container"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "secrets" {
-  type        = "map"
+  type        = map(string)
   description = "Secret credentials fetched using credstash"
   default     = {}
 }
 
 variable "dns_domain" {
-  type        = "string"
+  type        = string
   description = "The DNS domain"
 }
 
 variable "dns_name" {
-  type        = "string"
+  type        = string
   description = "The first part of the DNS name without the environment"
   default     = ""
 }
 
 variable "ecs_cluster" {
-  type        = "string"
+  type        = string
   description = "The ECS cluster"
   default     = "default"
 }
 
 variable "port" {
-  type        = "string"
+  type        = string
   description = "The port that container will be running on"
 }
 
 variable "cpu" {
-  type        = "string"
+  type        = string
   description = "CPU unit reservation for the container"
 }
 
 variable "memory" {
-  type        = "string"
+  type        = string
   description = "The memory reservation for the container in megabytes"
 }
 
 variable "alb_listener_arn" {
-  type        = "string"
+  type        = string
   description = "The Amazon Resource Name for the HTTPS listener on the load balancer"
 }
 
 variable "alb_dns_name" {
-  type        = "string"
+  type        = string
   description = "The DNS name of the load balancer"
 }
 
 variable "alb_listener_rule_priority" {
-  type        = "string"
+  type        = string
   description = "The priority for the rule - must be different per rule."
 }
 
 variable "desired_count" {
   description = "The number of instances of the task definition to place and keep running."
-  type        = "string"
+  type        = string
   default     = "3"
 }
 
 variable "logentries_token" {
   description = "The Logentries token used to be able to get logs sent to a specific log set."
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "deregistration_delay" {
   description = "The amount time for Elastic Load Balancing to wait before changing the state of a deregistering target from draining to unused. The range is 0-3600 seconds."
-  type        = "string"
+  type        = string
   default     = "10"
 }
 
 variable "health_check_interval" {
   description = "The approximate amount of time, in seconds, between health checks of an individual target. Minimum value 5 seconds, Maximum value 300 seconds."
-  type        = "string"
+  type        = string
   default     = "5"
 }
 
 variable "health_check_path" {
   description = "The destination for the health check request."
-  type        = "string"
+  type        = string
   default     = "/internal/healthcheck"
 }
 
 variable "health_check_timeout" {
   description = "The amount of time, in seconds, during which no response means a failed health check."
-  type        = "string"
+  type        = string
   default     = "4"
 }
 
 variable "health_check_healthy_threshold" {
   description = "The number of consecutive health checks successes required before considering an unhealthy target healthy."
-  type        = "string"
+  type        = string
   default     = "2"
 }
 
 variable "health_check_unhealthy_threshold" {
   description = "The number of consecutive health check failures required before considering the target unhealthy."
-  type        = "string"
+  type        = string
   default     = "2"
 }
 
 variable "health_check_matcher" {
   description = "The HTTP codes to use when checking for a successful response from a target. You can specify multiple values (for example, \"200,202\") or a range of values (for example, \"200-299\")."
-  type        = "string"
+  type        = string
   default     = "200-299"
 }
+
